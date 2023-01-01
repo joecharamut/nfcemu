88c481878c49ed34a0ce2837c432f33ee5c01aeb
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2023-01-01 17:11:38 -0500

    add license header

--- src/crc.c
+++ src/crc.c
@@ -1,5 +1,23 @@
+/***********************************************************************
+Copyright (C) 2022 Joseph Charamut
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***********************************************************************/
 #include "crc.h"
 
+// lookup table generated with http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
+// settings: CRC-16, Predefined[CRC16_A], Show reflected lookup table
 static const uint16_t CRC_TABLE[256] = {
   0x0000, 0x1189, 0x2312, 0x329B, 0x4624, 0x57AD, 0x6536, 0x74BF, 0x8C48, 0x9DC1, 0xAF5A, 0xBED3, 0xCA6C, 0xDBE5, 0xE97E, 0xF8F7,
   0x1081, 0x0108, 0x3393, 0x221A, 0x56A5, 0x472C, 0x75B7, 0x643E, 0x9CC9, 0x8D40, 0xBFDB, 0xAE52, 0xDAED, 0xCB64, 0xF9FF, 0xE876,
